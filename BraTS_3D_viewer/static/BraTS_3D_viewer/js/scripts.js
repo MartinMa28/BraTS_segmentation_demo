c0bc5f9c51e2c929ec41add8c5f25e86e513ee58
Martin <1135485226@qq.com>
2019-05-26 17:31:09 +0800

    use JSON.parse

--- BraTS_3D_viewer/static/BraTS_3D_viewer/js/scripts.js
+++ BraTS_3D_viewer/static/BraTS_3D_viewer/js/scripts.js
@@ -4,7 +4,7 @@ document.querySelectorAll('button.inference').forEach((btn) => {
         fetch(`http://10.253.218.12:8000/inference/${btn.dataset.caseName}`)
             .then(res => res.json())
             .then(data => {
-                console.log(JSON.stringify(data));
+                console.log(JSON.parse(data));
                 btn.textContent = 'Predict';
             })
             .catch((err) => {alert(err);});
@@ -19,8 +19,8 @@ document.querySelectorAll('button.view3D').forEach((btn) => {
         fetch(`http://10.253.218.12:8000/labels/${btn.dataset.caseName}`)
             .then(res => res.json())
             .then(data => {
-                console.log(JSON.stringify(data).length);
-                let inferenceLabels = JSON.stringify(data)
+                console.log(JSON.parse(data).length);
+                let inferenceLabels = JSON.parse(data)
                 let trace = {
                     x: inferenceLabels.xs,
                     y: inferenceLabels.ys,
