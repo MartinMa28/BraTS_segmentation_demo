30410208c866d6e7ed2fb3dd69975403bad8252c
Martin <1135485226@qq.com>
2019-05-26 16:36:39 +0800

    extract json fields

--- BraTS_3D_viewer/static/BraTS_3D_viewer/js/scripts.js
+++ BraTS_3D_viewer/static/BraTS_3D_viewer/js/scripts.js
@@ -4,7 +4,7 @@ document.querySelectorAll('button.inference').forEach((btn) => {
         fetch(`http://10.253.218.12:8000/inference/${btn.dataset.caseName}`)
             .then(res => res.json())
             .then(data => {
-                console.log(JSON.stringify(data).length);
+                console.log(JSON.stringify(data));
                 btn.textContent = 'Predict';
             })
             .catch((err) => {alert(err);});
@@ -19,7 +19,7 @@ document.querySelectorAll('button.view3D').forEach((btn) => {
         fetch(`http://10.253.218.12:8000/labels/${btn.dataset.caseName}`)
             .then(res => res.json())
             .then(data => {
-                console.log(JSON.stringify(data));
+                console.log(JSON.stringify(data).length);
                 btn.textContent = 'View';
             })
             .catch((err) => {
