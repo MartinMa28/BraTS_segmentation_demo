f5b0dd282a18afdc5e706c4bc1a53f138f6a9b49
Martin <1135485226@qq.com>
2019-05-26 16:58:35 +0800

    draw 3D scatter plot using Plotly.js

--- BraTS_3D_viewer/static/BraTS_3D_viewer/js/scripts.js
+++ BraTS_3D_viewer/static/BraTS_3D_viewer/js/scripts.js
@@ -20,6 +20,34 @@ document.querySelectorAll('button.view3D').forEach((btn) => {
             .then(res => res.json())
             .then(data => {
                 console.log(JSON.stringify(data).length);
+                let inferenceLabels = JSON.stringify(data)
+                let trace = {
+                    x: inferenceLabels.xs,
+                    y: inferenceLabels.ys,
+                    z: inferenceLabels.zs,
+                    mode: 'markers',
+                    markers: {
+                        size: 3,
+                        color: inferenceLabels.colors,
+                        colorscale: 'Viridis',
+                        opacity: 0.8
+                    },
+                    type: 'scatter3d'
+                };
+
+                let scatterData = [trace];
+                
+                let layout = {
+                    margin: {
+                        l: 0,
+                        r: 0,
+                        b: 0,
+                        t: 0
+                    }
+                };
+
+                Plotly.newPlot('3D_viewer_container', scatterData, layout);
+                
                 btn.textContent = 'View';
             })
             .catch((err) => {
