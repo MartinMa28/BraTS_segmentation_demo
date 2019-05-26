79c05e81cea06ceeaeeec41435bc42cccc9f1dbb
Martin <1135485226@qq.com>
2019-05-26 16:20:37 +0800

    test for fetch labels RESTful API

--- BraTS_3D_viewer/static/BraTS_3D_viewer/js/scripts.js
+++ BraTS_3D_viewer/static/BraTS_3D_viewer/js/scripts.js
@@ -3,7 +3,7 @@ document.querySelectorAll('button.inference').forEach((btn) => {
         console.log("inference " + btn.dataset.caseName);
         fetch(`http://10.253.218.12:8000/inference/${btn.dataset.caseName}`)
             .then((res) => {
-                alert(res);
+                console.log(res.json());
                 btn.textContent = "Predict";
             })
             .catch((err) => {alert(err);});
@@ -15,5 +15,15 @@ document.querySelectorAll('button.inference').forEach((btn) => {
 document.querySelectorAll('button.view3D').forEach((btn) => {
     btn.addEventListener('click', () => {
         console.log("3D view " + btn.dataset.caseName);
+        fetch(`http://10.253.218.12:8000/labels/${btn.dataset.caseName}`)
+            .then((res) => {
+                console.log(`length: ${res.json().length}`);
+                btn.textContent = 'View';
+            })
+            .catch((err) => {
+                alert(err);
+            });
+        
+        btn.textContent = 'Plotting...';
     });
 });
