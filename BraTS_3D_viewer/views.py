2a7a3b57bbe319cff97d9dac454a2f452f3d86a5
Martin <1135485226@qq.com>
2019-06-04 16:04:51 +0800

    added device to memory_cached and memory_allocated

--- BraTS_3D_viewer/views.py
+++ BraTS_3D_viewer/views.py
@@ -133,11 +133,13 @@ def infer(request, case_name):
     del slice_i
     del pred
     del output
-    del device
+    
     torch.cuda.empty_cache()
     
-    print(f'memory allocated: {torch.cuda.memory_allocated()}')
-    print(f'memory cached: {torch.cuda.memory_cached()}')
+    print(f'memory allocated: {torch.cuda.memory_allocated(device)}')
+    print(f'max memory allocated: {torch.cuda.max_memory_allocated(device)}')
+    print(f'memory cached: {torch.cuda.memory_cached(device)}')
+    print(f'max memory cached: {torch.cuda.max_memory_cached(device)}')
 
     return JsonResponse({'labels': preds})
 
